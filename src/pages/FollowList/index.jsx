93667ff7a0b4844918cbfd4312ece4aa8cc2433c
wuju <deppsd0@gmail.com>
2024-01-23 16:15:01 +0900

    Design: FollowList 네비바 수정

--- src/pages/FollowList/index.jsx
+++ src/pages/FollowList/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// 우주 수정 중
 
 import { Button, Img, List, Text, MainHeader, Navbar } from "components";
 import FollowListFollowuserrow from "components/FollowListFollowuserrow";
@@ -13,19 +14,19 @@ const FollowListPage = () => {
 
   return (
     <>
-      <div className="bg-white-A700 flex flex-col font-inter items-center justify-start mx-auto w-auto sm:w-full md:w-full">
+      <div className="flex flex-col items-center justify-start w-auto mx-auto bg-white-A700 font-inter sm:w-full md:w-full">
         <MainHeader className="bg-white-A700 flex flex-col items-center justify-start p-[25px] sm:px-5 w-full" />
         <Navbar className="max-w-[1180px] mx-auto w-full" />
         <div className="border-b border-gray-300 border-solid flex flex-col gap-5 h-[603px] md:h-auto items-center justify-start max-w-[720px] pt-[30px] md:px-5 w-full">
-          <div className="flex flex-row sm:gap-10 gap-20 items-start justify-center outline outline-gray-300 w-full">
-            <div className="flex flex-col items-center justify-center pb-2.5 w-auto">
-              <Text
-                className="text-[22px] text-blue_gray-100_01 text-center sm:text-lg md:text-xl tracking-[-0.42px] w-auto"
-                size="txtInterMedium22"
-              >
-                팔로워
-              </Text>
-            </div>
+          <div className="flex flex-row items-start justify-center w-full gap-20 border-b border-gray-300 border-solid sm:gap-10">
+            <Button
+              className="cursor-pointer font-medium min-w-[100px] text-[22px] text-center sm:text-lg md:text-xl tracking-[-0.42px] text-blue_gray-100_01"
+              shape="square"
+              size="sm"
+              variant="outline"
+            >
+              팔로워
+            </Button>
             <Button
               className="cursor-pointer font-medium min-w-[100px] text-[22px] text-center sm:text-lg md:text-xl tracking-[-0.42px]"
               shape="square"
@@ -37,13 +38,13 @@ const FollowListPage = () => {
             </Button>
           </div>
           <List
-            className="flex flex-col gap-5 items-center pb-5 w-full"
+            className="flex flex-col items-center w-full gap-5 pb-5 "
             orientation="vertical"
           >
             {followListFollowuserrowPropList.map((props, index) => (
               <React.Fragment key={`FollowListFollowuserrow${index}`}>
                 <FollowListFollowuserrow
-                  className="flex flex-1 sm:flex-col flex-row gap-5 items-center justify-center my-0 w-full"
+                  className="flex flex-row items-center justify-center flex-1 w-full gap-5 my-0 sm:flex-col"
                   {...props}
                 />
               </React.Fragment>
