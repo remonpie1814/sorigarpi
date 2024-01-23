83f9805adb9aacdaccb856a691f67599b63cc97c
wuju <deppsd0@gmail.com>
2024-01-23 15:49:18 +0900

    Design: FollowListFollowuserrow 디자인 수정
    - 팔로우한 사람 이름 가로줄 맞춤
    - 버튼 크기 수정

--- src/components/FollowListFollowuserrow/index.jsx
+++ src/components/FollowListFollowuserrow/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+// 우주 수정 중
 
-import { Img, Text } from "components";
+import { Button, Img, Text } from "components";
 
 const FollowListFollowuserrow = (props) => {
   return (
@@ -15,27 +16,26 @@ const FollowListFollowuserrow = (props) => {
         </div>
         <div className="flex flex-col items-start justify-start pr-0.5 py-0.5">
           <Text
-            className="mb-[11px] text-base text-gray-900 tracking-[-0.30px]"
+            className="text-base text-gray-900 tracking-[-0.30px]"
             size="txtInterMedium16"
           >
             {props?.followtext}
           </Text>
         </div>
-        <div className="flex flex-col items-center justify-start px-[9px] w-[7%] sm:w-full">
+        <div className="flex flex-col items-center justify-center px-[9px] w-[7%] sm:w-full">
           <Img
-            className="h-[30px] md:h-auto object-cover w-[30px]"
+            className="h-[23px] md:h-auto object-cover w-[23px]"
             src="images/img__30x30.png"
             alt="FiftyOne"
           />
         </div>
-        <div className="h-[103px] md:h-[35px] py-[34px] relative w-[17%] sm:w-full">
-          <div className="absolute bg-amber-A100 h-[35px] inset-[0] justify-center m-auto rounded-[10px] w-full"></div>
-          <Text
-            className="absolute h-max inset-[0] justify-center m-auto text-base text-black-900 text-center tracking-[-0.30px] w-max"
+        <div className="h-[103px] md:h-[35px] py-[34px] relative w-[17%] sm:w-full ">
+          <Button
+            className="absolute bg-amber-A100 h-[35px] w-[100%]	 inset-[0] justify-center m-auto rounded-[10px] text-base text-black-900 text-center tracking-[-0.30px] w-max"
             size="txtInterMedium16Black900"
           >
             {props?.deletetext}
-          </Text>
+          </Button>
         </div>
       </div>
     </>
