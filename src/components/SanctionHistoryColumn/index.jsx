2bf8b3826463ab37c981b5b003e651d503ad2e53
yeji1814 <yeji1814@gmail.com>
2024-01-26 13:51:40 +0900

    Design: SanctionHistory 정렬

--- src/components/SanctionHistoryColumn/index.jsx
+++ src/components/SanctionHistoryColumn/index.jsx
@@ -7,16 +7,16 @@ const SanctionHistoryColumn = (props) => {
     <>
       <div className={props.className}>
         <div className="flex md:flex-col flex-row md:gap-5 items-start justify-start max-w-[1180px] w-full">
-          <Button
-            className="!text-white-A700 border-gray-400_03 border-l border-solid border-t cursor-pointer flex-1 font-bold font-inter h-[67px] text-[22px] text-center sm:text-lg md:text-xl w-full"
-            shape="square"
-            color="gray_600"
-            size="2xl"
-            variant="fill"
+          <div
+            className="flex justify-center items-center
+            bg-gray-600 
+            border-gray-400_03 border-l border-solid border-t
+            !text-white-A700 font-bold font-inter h-[67px] text-[22px] text-center
+            sm:text-lg md:text-xl w-1/2 md:w-full"
           >
-            {props?.buttontext1}
-          </Button>
-          <div className="border-gray-400_03 border-l border-solid border-t flex flex-1 flex-col h-[67px] md:h-auto items-start justify-start max-w-[773px] w-full">
+            항목
+          </div>
+          <div className="border-gray-400_03 border-l border-solid border-t flex flex-1 flex-col h-[67px] md:h-auto items-start justify-start w-full">
             <div className="flex flex-col h-full items-center justify-center px-3 py-2.5 w-full">
               <Text
                 className="text-base text-black-900 w-auto"
@@ -28,16 +28,16 @@ const SanctionHistoryColumn = (props) => {
           </div>
         </div>
         <div className="flex md:flex-col flex-row md:gap-5 items-start justify-start max-w-[1180px] w-full">
-          <Button
-            className="!text-white-A700 border-gray-400_03 border-l border-solid border-t cursor-pointer flex-1 font-bold font-inter h-[67px] text-[22px] text-center sm:text-lg md:text-xl w-full"
-            shape="square"
-            color="gray_600"
-            size="2xl"
-            variant="fill"
+          <div
+            className="flex justify-center items-center
+            bg-gray-600 
+            border-gray-400_03 border-l border-solid border-t
+            !text-white-A700 font-bold font-inter h-[67px] text-[22px] text-center
+            sm:text-lg md:text-xl w-1/2 md:w-full"
           >
-            {props?.buttontext2}
-          </Button>
-          <div className="bg-white-A700_0f border-gray-400_03 border-l border-solid border-t flex flex-1 flex-col h-[67px] md:h-auto items-start justify-start max-w-[773px] w-full">
+            내용
+          </div>
+          <div className="bg-white-A700_0f border-gray-400_03 border-l border-solid border-t flex flex-1 flex-col h-[67px] md:h-auto items-start justify-start w-full">
             <div className="flex flex-col h-full items-center justify-center px-3 py-2.5 w-full">
               <Text
                 className="text-base text-black-900 w-auto"
@@ -49,16 +49,16 @@ const SanctionHistoryColumn = (props) => {
           </div>
         </div>
         <div className="flex md:flex-col flex-row md:gap-5 h-[66px] md:h-auto items-start justify-start max-w-[1180px] w-full">
-          <Button
-            className="!text-white-A700 border-gray-400_03 border-l border-solid border-t cursor-pointer flex-1 font-bold font-inter h-[66px] text-[22px] text-center sm:text-lg md:text-xl w-full"
-            shape="square"
-            color="gray_600"
-            size="xl"
-            variant="fill"
+          <div
+            className="flex justify-center items-center
+            bg-gray-600 
+            border-gray-400_03 border-l border-solid border-t
+            !text-white-A700 font-bold font-inter h-[67px] text-[22px] text-center
+            sm:text-lg md:text-xl w-1/2 md:w-full"
           >
-            {props?.buttontext3}
-          </Button>
-          <div className="border-gray-400_03 border-l border-solid border-t flex flex-1 flex-col h-[66px] md:h-auto items-start justify-start max-w-[773px] w-full">
+            일시
+          </div>
+          <div className="border-gray-400_03 border-l border-solid border-t flex flex-1 flex-col h-[66px] md:h-auto items-start justify-start w-full">
             <div className="flex flex-col h-full items-center justify-center px-3 py-2.5 w-full">
               <Text
                 className="text-base text-black-900 w-auto"
@@ -70,16 +70,16 @@ const SanctionHistoryColumn = (props) => {
           </div>
         </div>
         <div className="flex md:flex-col flex-row md:gap-5 h-[67px] md:h-auto items-start justify-start max-w-[1180px] w-full">
-          <Button
-            className="!text-white-A700 border-gray-400_03 border-l border-solid border-t cursor-pointer flex-1 font-bold font-inter h-[67px] text-[22px] text-center sm:text-lg md:text-xl w-full"
-            shape="square"
-            color="gray_600"
-            size="2xl"
-            variant="fill"
+          <div
+            className="flex justify-center items-center
+            bg-gray-600 
+            border-gray-400_03 border-l border-solid border-t
+            !text-white-A700 font-bold font-inter h-[67px] text-[22px] text-center
+            sm:text-lg md:text-xl w-1/2 md:w-full"
           >
-            {props?.buttontext4}
-          </Button>
-          <div className="border-gray-400_03 border-l border-solid border-t flex flex-1 flex-col h-[67px] md:h-auto items-start justify-start max-w-[773px] w-full">
+            상세사유
+          </div>
+          <div className="border-gray-400_03 border-l border-solid border-t flex flex-1 flex-col h-[67px] md:h-auto items-start justify-start w-full">
             <div className="flex flex-col h-full items-center justify-center px-3 py-2.5 w-full">
               <Text
                 className="text-base text-black-900 w-auto"
@@ -96,13 +96,9 @@ const SanctionHistoryColumn = (props) => {
 };
 
 SanctionHistoryColumn.defaultProps = {
-  buttontext1: "항목",
   text1: "욕설/비방이 적힌 댓글",
-  buttontext2: "내용",
   text2: "미칀룐이세욘 ㅋㅋ",
-  buttontext3: "일시",
   text3: "2023-11-24 12:23",
-  buttontext4: "상세 사유",
   text4: "왜인지 잘 아시죠",
 };
 
