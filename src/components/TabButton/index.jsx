c1f532b0e00b87b1e9cc90f5b065cdacbe81e342
wuju <deppsd0@gmail.com>
2024-02-03 15:44:35 +0900

    Refactor: TapButton 컴포넌트에 border props추가
    -FollowList에 TapButton 추가하면서 border 디자인 요소를 추가하기 위해 TapButton에 border props 추가
    -CustomerService에 원래 적용하던 border 디자인을 props로 전달할 수 있게 수정

--- src/components/TabButton/index.jsx
+++ src/components/TabButton/index.jsx
@@ -1,12 +1,12 @@
 import { Button } from "components";
 
-const TabButton = ({ className, children, index, tab, setTab }) => {
+const TabButton = ({ className, children, index, tab, setTab, border }) => {
   return (
     <Button
       className={`cursor-pointer min-w-[100px]
                  text-base text-center tracking-[-0.30px]
-                 ${className}
-                ${tab === index ? "border-b-2 font-bold" : null}`}
+                 ${className} 
+                ${tab === index ? border + " font-bold" : null}`}
       shape="square"
       size="xs"
       onClick={() => {
--- src/pages/FollowList/index.jsx
+++ src/pages/FollowList/index.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-// 우주 수정 중
+import React, { useState } from "react";
 
-import { Button, Img, List, Text, MainHeader, Navbar } from "components";
+import { Button, Img, List, Text, Layout, TabButton } from "components";
 import FollowListFollowuserrow from "components/FollowListFollowuserrow";
 
 const FollowListPage = () => {
@@ -12,30 +11,29 @@ const FollowListPage = () => {
     { followtext: "콩솔이" },
   ];
 
+  const [tab, setTab] = useState(0);
+
   return (
     <>
-      <div className="flex flex-col items-center justify-start w-auto mx-auto bg-white-A700 font-inter sm:w-full md:w-full">
-        <MainHeader className="bg-white-A700 flex flex-col items-center justify-start p-[25px] sm:px-5 w-full" />
-        <Navbar className="max-w-[1180px] mx-auto w-full" />
+      <Layout>
         <div className="border-b border-gray-300 border-solid flex flex-col gap-5 h-[603px] md:h-auto items-center justify-start max-w-[720px] pt-[30px] md:px-5 w-full">
           <div className="flex flex-row items-start justify-center w-full gap-20 border-b border-gray-300 border-solid sm:gap-10">
-            <Button
-              className="cursor-pointer font-medium min-w-[100px] text-[22px] text-center sm:text-lg md:text-xl tracking-[-0.42px] text-blue_gray-100_01"
-              shape="square"
-              size="sm"
-              variant="outline"
-            >
-              팔로워
-            </Button>
-            <Button
-              className="cursor-pointer font-medium min-w-[100px] text-[22px] text-center sm:text-lg md:text-xl tracking-[-0.42px]"
-              shape="square"
-              color="amber_A100"
-              size="sm"
-              variant="outline"
+            <TabButton
+              index={0}
+              tab={tab}
+              setTab={setTab}
+              border={"border-amber-A100 border-b-[5px] border-solid"}
             >
               팔로우
-            </Button>
+            </TabButton>
+            <TabButton
+              index={1}
+              tab={tab}
+              setTab={setTab}
+              border={"border-amber-A100 border-b-[5px] border-solid"}
+            >
+              팔로워
+            </TabButton>
           </div>
           <List
             className="flex flex-col items-center w-full gap-5 pb-5 "
@@ -51,7 +49,7 @@ const FollowListPage = () => {
             ))}
           </List>
         </div>
-      </div>
+      </Layout>
     </>
   );
 };
