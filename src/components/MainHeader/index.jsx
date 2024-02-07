a273660f03c6530fb76199eb97bad688e0ef8daa
yeji1814 <yeji1814@gmail.com>
2024-02-07 22:07:15 +0900

    Chore: 브라우저가 sm 크기일 때 헤더의 버튼, 로고 정렬방식 세로 -> 가로로 변경

--- src/components/MainHeader/index.jsx
+++ src/components/MainHeader/index.jsx
@@ -10,7 +10,7 @@ const MainHeader = (props) => {
   return (
     <>
       <div className={props.className}>
-        <div className="flex flex-row items-center justify-between w-[64%] md:w-full md:flex-col md:gap-10">
+        <div className="flex flex-row items-center justify-between w-[64%] md:w-full md:gap-10">
           <div className="flex flex-col items-center justify-center w-auto">
             <Img
               className="h-[51px] md:h-auto object-cover w-[170px]"
@@ -18,7 +18,7 @@ const MainHeader = (props) => {
               alt="sorigalpi logo"
             />
           </div>
-          <div className="flex flex-row gap-2.5 items-center justify-center py-2.5 w-auto sm:w-full sm:flex-col">
+          <div className="flex flex-row gap-2.5 items-center justify-center py-2.5 w-auto">
             <Img
               className="h-12 md:h-auto object-cover w-12"
               src="images/img_bell111.png"
