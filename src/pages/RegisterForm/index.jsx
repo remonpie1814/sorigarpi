eee14e8f352b42f4bc93c23f5b4c0cd202c0bea8
yeji1814 <yeji1814@gmail.com>
2024-02-19 20:07:34 +0900

    Refactor: RegisterForm 회원가입 함수 추가
    - 잘못된 입력이면 회원가입을 눌렀을 때 더이상 진행되지 않고 input에 Focus.
    - 실제로 진행하는 로직은 비어있음

--- src/pages/RegisterForm/index.jsx
+++ src/pages/RegisterForm/index.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
-import { Button, CheckBox, Img, Input, Layout, Text } from "components";
+import { Button, CheckBox, Img, Input, Layout, Text, Toast } from "components";
 
 const RegisterFormPage = () => {
   const [email, setEmail] = useState("");
@@ -8,10 +8,39 @@ const RegisterFormPage = () => {
   const [password2, setPassword2] = useState("");
   const [nickName, setNickName] = useState("");
 
+  // 참조 생성
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const password2Ref = useRef(null);
+
   //정규식
   let emailRegex =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   let pwdRegex = /^(?=.*[a-zA-Z])(?=.*\d).*$/;
+
+  function checkValidation() {
+    if (!emailRegex.test(email)) {
+      emailRef.current.focus();
+      return false;
+    }
+    if (password.length < 4 || !pwdRegex.test(password)) {
+      passwordRef.current.focus();
+      return false;
+    }
+    if (password2 !== password) {
+      password2Ref.current.focus();
+      return false;
+    }
+    return true;
+  }
+
+  // todo: 회원가입 버튼 동작 함수
+  function handleRegisterSubmit() {
+    const isValidate = checkValidation();
+    if (isValidate) {
+      console.log("회원가입 진행");
+    }
+  }
 
   return (
     <>
@@ -29,6 +58,7 @@ const RegisterFormPage = () => {
           <div className="flex flex-col items-center justify-start w-full">
             <Input
               name="input"
+              ref={emailRef}
               placeholder="이메일"
               className="font-medium p-0 placeholder:text-black-900 text-base text-left tracking-[-0.30px] w-full"
               wrapClassName="w-full"
@@ -44,6 +74,7 @@ const RegisterFormPage = () => {
             </ErrMSG>
             <Input
               type="password"
+              ref={passwordRef}
               placeholder="비밀번호"
               className="font-medium p-0 placeholder:text-black-900 text-base text-left tracking-[-0.30px] w-full"
               wrapClassName="w-full"
@@ -64,6 +95,7 @@ const RegisterFormPage = () => {
             </ErrMSG>
             <Input
               type="password"
+              ref={password2Ref}
               placeholder="비밀번호 확인"
               className="font-medium p-0 placeholder:text-black-900 text-base text-left tracking-[-0.30px] w-full"
               wrapClassName="w-full"
@@ -93,7 +125,7 @@ const RegisterFormPage = () => {
               <CheckBox
                 className="flex flex-row justify-center items-center font-medium text-base tracking-[-0.30px] w-full text-center"
                 inputClassName="w-5 h-5 mr-2 outline outline-[2px] outline-gray-300 rounded-sm"
-                name="ThreeHundredEightyNine"
+                name="agreement"
                 id="ThreeHundredEightyNine"
                 label="개인정보 이용동의"
                 color="white_A700"
@@ -104,6 +136,9 @@ const RegisterFormPage = () => {
           <Button
             className="cursor-pointer font-medium w-full rounded-[10px]
                       text-[22px] text-center sm:text-lg md:text-xl tracking-[-0.42px]"
+            onClick={() => {
+              handleRegisterSubmit();
+            }}
             color="amber_A100"
             size="xl"
             variant="fill"
