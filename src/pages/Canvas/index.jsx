91ee6cb4c3c868f37789575d72ac90f955f22836
yeji1814 <yeji1814@gmail.com>
2024-03-01 12:54:51 +0900

    feat: 표시 페이지 바꾸기
    각 페이지의 id와 history 배열(undo를 위한 배열)을 Canvas에 state로 전달하는 방식
    페이지를 찾을 때 배열의 index를 참조하지 않고 id를 참조하는 것은 나중에 페이지 목록에서 각 페이지의 순서를 바꾸거나 할 때 배열에서 index를 바꿀지도 모른단 생각에.

--- src/pages/Canvas/index.jsx
+++ src/pages/Canvas/index.jsx
@@ -5,18 +5,51 @@ import { Sidebar } from "react-pro-sidebar";
 import { Button, Img, Text } from "components";
 
 const CanvasPage = () => {
+  // 도구 state
+  const [currentTool, setCurrentTool] = useState("pen");
+  // 현재 페이지 state
+  const [currentPage, setCurrentPage] = useState("page-1-id");
+  // 현재 히스토리
+  const [currentHistory, setCurrentHistory] = useState([]);
+  // 페이지들의 state
+  const [pages, setPages] = useState([
+    { pageId: "page-1-id" },
+    { pageId: "page-2-id" },
+  ]);
+
+  function changePage(toId) {
+    saveCanvas(currentPage.pageId, currentHistory);
+    const target = pages.find((page) => page.pageId === toId);
+    setCurrentHistory(target.savedHistory == null ? [] : target.savedHistory);
+    setCurrentPage(target.pageId);
+    console.log("change to pageid = " + toId);
+  }
+
+  function saveCanvas(id, value) {
+    // 특정 id를 가진 원소를 제거하고 새 원소를 추가하여 업데이트된 배열을 생성
+    const updatedPages = pages.filter((page) => page.pageId !== id);
+    updatedPages.push({ pageId: id, savedHistory: [...value] });
+    // 상태를 업데이트
+    setPages(updatedPages);
+
+    pages.map((page) => {
+      console.log("canvas saved.");
+      console.log(page);
+    });
+  }
+
   document.body.style = "overflow:hidden";
   return (
     <>
       <div className="bg-white-A700 flex flex-col font-inter items-center justify-start h-screen">
         <div className="flex flex-col items-start justify-start md:px-10 sm:px-5 px-[50px] w-full">
-          <div className="flex md:flex-col flex-row md:gap-10 items-center justify-between mx-auto py-[5px] w-full">
+          <div className="flex flex-row items-center justify-between mx-auto py-[5px] w-full">
             <Img
               className="h-[30px] w-[260px]"
               src="images/img__black_900_30x260.svg"
               alt="FourHundredThirtySix"
             />
-            <div className="flex sm:flex-1 sm:flex-col flex-row gap-5 items-start justify-start w-auto sm:w-full">
+            <div className="flex flex-row gap-5 items-start justify-start w-auto sm:w-full">
               <Img
                 className="h-[30px] md:h-auto object-cover w-[30px]"
                 src="images/img_image689.png"
@@ -54,7 +87,12 @@ const CanvasPage = () => {
             <div className="flex flex-col h-full items-start justify-center mx-auto my-[9px] px-5 py-2.5 w-full">
               <div className="flex flex-col gap-5 items-center justify-start w-[92%]">
                 <div className="flex flex-row items-center justify-between w-full">
-                  <div className="flex flex-col gap-2.5 items-center justify-start w-[130px]">
+                  <div
+                    className="flex flex-col gap-2.5 items-center justify-start w-[130px]"
+                    onClick={() => {
+                      changePage("page-1-id");
+                    }}
+                  >
                     <Img
                       className="h-[175px] w-[130px]"
                       src="images/img__deep_orange_100.svg"
@@ -67,7 +105,12 @@ const CanvasPage = () => {
                       1페이지
                     </Text>
                   </div>
-                  <div className="flex flex-col gap-2.5 items-center justify-start w-[130px]">
+                  <div
+                    className="flex flex-col gap-2.5 items-center justify-start w-[130px]"
+                    onClick={() => {
+                      changePage("page-2-id");
+                    }}
+                  >
                     <Img
                       className="h-[175px] w-[130px]"
                       src="images/img__deep_orange_100.svg"
@@ -168,8 +211,12 @@ const CanvasPage = () => {
               </div>
             </div>
           </Sidebar>
-          <Canvas />
-          <CanvasRowthumbsup className="fixed right-0 flex sm:gap-10 items-start" />
+          <Canvas
+            pageId={currentPage}
+            history={currentHistory}
+            setCurrentHistory={setCurrentHistory}
+          />
+          <CanvasRowthumbsup className="flex sm:gap-10 items-start" />
         </div>
       </div>
     </>
@@ -334,11 +381,10 @@ const CanvasRowthumbsup = (props) => {
   );
 };
 
-const Canvas = () => {
+const Canvas = ({ pageId, history, currentTool }) => {
   const canvasRef = useRef(null);
   const [getCtx, setGetCtx] = useState(null);
   const [painting, setPainting] = useState(false);
-  const [tracking, setTracking] = useState([]);
 
   const drawFn = (e) => {
     const mouseX = e.nativeEvent.offsetX;
@@ -352,6 +398,37 @@ const Canvas = () => {
     }
   };
 
+  const onMouseDownHandler = () => {
+    // todo: 이 아래에 각 도구별 마우스를 클릭했을 때의 동작 채워야 함
+    if (currentTool == "pen") {
+      setPainting(true);
+    }
+  };
+
+  const onMouseUpHandler = () => {
+    // 각 도구별 마우스를 뗐을 때의 동작
+    if (currentTool == "pen") {
+      if (painting) {
+        history.push(
+          getCtx.getImageData(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+          )
+        );
+      }
+      setPainting(false);
+    }
+  };
+
+  const onMouseMoveHandler = (e) => {
+    // 각 도구별 마우스를 클릭한 채 움직일 때의 동작
+    if (currentTool == "pen") {
+      drawFn(e);
+    }
+  };
+
   useEffect(() => {
     const canvas = canvasRef.current;
     canvas.width = 650;
@@ -361,15 +438,20 @@ const Canvas = () => {
     ctx.lineWidth = 2.5;
     ctx.strokeStyle = "#000000";
     setGetCtx(ctx);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (history.length > 0) {
+      ctx.putImageData(history[history.length - 1], 0, 0);
+    }
 
     const onKeyDown = (e) => {
       // Control+Z 조합이 눌렸을 때 함수 실행
       if (e.ctrlKey && e.key === "z") {
-        if (tracking.length > 0) {
+        console.log(history.length);
+        if (history.length > 0) {
           // 가장 최근 상태를 제거하고, 이전 상태로 canvas를 복원
-          tracking.pop();
-          if (tracking.length > 0) {
-            ctx.putImageData(tracking[tracking.length - 1], 0, 0);
+          history.pop();
+          if (history.length > 0) {
+            ctx.putImageData(history[history.length - 1], 0, 0);
           } else {
             // 모든 그림이 제거되었을 경우, canvas를 비우기
             ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -379,51 +461,36 @@ const Canvas = () => {
     };
 
     document.addEventListener("keydown", onKeyDown);
-
+    console.log(pageId + "mounted");
     // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
     return () => {
       document.removeEventListener("keydown", onKeyDown);
+      console.log(pageId + "unmounted");
     };
-  }, []);
+  }, [pageId]);
 
   return (
     <div
       id="canvas-view"
       className="flex flex-col flex-grow justify-center items-center
-      w-full h-screen overflow-scroll"
+      h-screen overflow-scroll"
     >
       <div id="canvas-wrap" className="">
-        {tracking.length}
+        {history.length}
         <canvas
           className="bg-white-A700"
           ref={canvasRef}
-          onMouseDown={() => setPainting(true)}
-          onMouseUp={() => {
-            if (painting) {
-              tracking.push(
-                getCtx.getImageData(
-                  0,
-                  0,
-                  canvasRef.current.width,
-                  canvasRef.current.height
-                )
-              );
-            }
-            setPainting(false);
+          onMouseDown={() => {
+            onMouseDownHandler();
           }}
-          onMouseMove={(e) => drawFn(e)}
+          onMouseUp={() => {
+            onMouseUpHandler();
+          }}
+          onMouseMove={(e) => {
+            onMouseMoveHandler(e);
+          }}
           onMouseLeave={() => {
-            if (painting) {
-              tracking.push(
-                getCtx.getImageData(
-                  0,
-                  0,
-                  canvasRef.current.width,
-                  canvasRef.current.height
-                )
-              );
-            }
-            setPainting(false);
+            onMouseUpHandler();
           }}
         ></canvas>
       </div>
